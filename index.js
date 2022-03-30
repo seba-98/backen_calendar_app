15ce418be98821d4296d1a68dbb7ebfa08a00885
sebastian belettieri <sebastianbelettieri1@gmail.com>
2022-03-30 13:42:30 -0300

    modified index.js

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth' ));
 app.use('/api/events', require('./routes/events'));
 
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/public'));
-});
+// app.get('*', (req,res) =>{
+//     res.sendFile(path.join(__dirname+'/public'));
+// });
 
 //escuchar peticiones
 app.listen(process.env.PORT, ()=>{
