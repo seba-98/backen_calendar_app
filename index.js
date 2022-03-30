694bfe296936cf9f1f440653ea6c398700a5a0ff
sebastian belettieri <sebastianbelettieri1@gmail.com>
2022-03-30 13:00:20 -0300

    replace dist for build in public folder

--- index.js
+++ index.js
@@ -19,12 +19,14 @@ app.use(express.static('public'));
 app.use(express.json())
 
 //rutas
-
-app.use('*', express.static('public'));
-app.use('/login', express.static('public'));
+// app.use('*', express.static('public'));
+// app.use('/login', express.static('public'));
 app.use('/api/auth', require('./routes/auth' ));
 app.use('/api/events', require('./routes/events'));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html'));
+  });
 
 //escuchar peticiones
 app.listen(process.env.PORT, ()=>{
