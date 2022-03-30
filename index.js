44b4fc52f4fc521ea498dfb4590dfd1ec82b7958
sebastian belettieri <sebastianbelettieri1@gmail.com>
2022-03-30 13:54:41 -0300

    modified index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use('/api/auth', require('./routes/auth' ));
 app.use('/api/events', require('./routes/events'));
 
 app.get('/*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
 //escuchar peticiones
